49dfdb3d6a195a0579698dbbd643e5b8aa9dcd73
Vlad Lyshchuk <47116424+acarrizo228@users.noreply.github.com>
2019-02-17 18:29:25 +0200

    Initial commit

---
+++ src/com/epam/workspace/Main.java
@@ -0,0 +1,13 @@
+package com.epam.workspace;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("****\t" + "*****\t" + "\t*\t\t" + "*\t\t*");
+        System.out.println("*\t\t" + "*\t*\t" + "   * *\t\t" + "* *  *  *");
+        System.out.println("**\t\t" + "*****\t" + "  *   *\t\t" + "*\t*\t*");
+        System.out.println("*\t\t" + "*\t   \t " + "*******\t" + "*\t\t*");
+        System.out.println("****\t" + "*\t    " + "*\t\t*\t" + "*\t\t*");
+        System.out.println("\n\n\t (c) Vlad Lyshchuk");
+    }
+}
